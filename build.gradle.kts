2401be0bcc655ebfe241f4a01f3a1fb13c101a7d
Dizon <john.patrick.dizon@accenture.com>
2025-10-24 16:31:17 +0800

    added spring-session-jdbc to check vulnerability

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
 	implementation("org.springframework.security:spring-security-saml2-service-provider")
+	implementation("org.springframework.session:spring-session-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
